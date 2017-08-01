62cd6417ca7e2f1a93c53bed4ecb043c7b6ce7fd
Angel Cortez <acort3255@gmail.com>
2017-08-01 14:12:49 -0400

    Add data manager and fixed minor bugs

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -2,7 +2,134 @@
 {
 ?>
 <?php
-    $form;
+    // To start buffered output for header redirecting
+    ob_start();
+    //
+    
+    class APIDataManager {
+        // The database connection
+        protected static $connection;
+
+        /**
+        * Connect to the database
+        * 
+        * @return bool false on failure / mysqli MySQLi object instance on success
+        */
+        public function connect() {    
+        // Try and connect to the database
+            if(!isset(self::$connection)) {
+                // Load configuration as an array. Use the actual location of your configuration file
+                $config = parse_ini_file('./config.ini');
+                self::$connection = new mysqli('127.0.0.1',$config['username'],$config['password'],$config['dbname']);
+            }
+
+            // If connection was not successful, handle the error
+            if(self::$connection === false) {
+                // Handle error - notify administrator, log to a file, show an error screen, etc.
+                return false;
+            }
+            return self::$connection;
+        }
+        
+        /**
+        * Insert into the database
+        *
+        * @param $firstName The first name string
+        * @param $lastName The last name string
+        * @param $email The email string
+        * @return mixed The result of the mysqli::query() function
+        */
+        public function insertClient($firstName, $lastName, $email) {
+            // Connect to the database
+            $connection = $this->connect();
+            $stmt = "";
+            $results = "Failed to prepare statement!";
+            
+            if ($stmt = $connection->prepare("INSERT INTO `Basement_Systems`.`Client` (`Email`, `firstName`, `lastName`, `date`) VALUES (?, ?, ?, NOW());")) {
+
+                /* bind parameters for markers */
+                $stmt->bind_param("sss", $email, $firstName, $lastName);
+
+                /* execute query */
+                $stmt->execute();
+                
+                if ($connection->affected_rows != -1)
+                {
+                    return "success";
+                }
+                else
+                {
+                    $results = "Couldn't insert into database!";
+                }
+                
+                /* close statement */
+                $stmt->close();
+            }
+            
+            return "error: ".$results;
+        }
+
+        /**
+        * Query the database
+        *
+        * @param $query The query string
+        * @return mixed The result of the mysqli::query() function
+        */
+        public function query($query) {
+            // Connect to the database
+            $connection = $this -> connect();
+
+            // Query the database
+            $result = $connection -> query($query);
+
+            return $result;
+        }
+
+        /**
+        * Fetch rows from the database (SELECT query)
+        *
+        * @param $query The query string
+        * @return bool False on failure / array Database rows on success
+        */
+        public function select($query) {
+            $rows = array();
+            $result = $this -> query($query);
+            if($result === false) {
+            return false;
+            }
+            while ($row = $result -> fetch_assoc()) {
+                $rows[] = $row;
+            }
+            return $rows;
+        }
+
+        /**
+        * Fetch the last error from the database
+        * 
+        * @return string Database error message
+        */
+        public function error() {
+        $connection = $this -> connect();
+        return $connection -> error;
+        }
+
+        /**
+        * Quote and escape value for use in a database query
+        *
+        * @param string $value The value to be quoted and escaped
+        * @return string The quoted and escaped string
+        */
+        public function quote($value) {
+            $connection = $this -> connect();
+            return "'" . $connection -> real_escape_string($value) . "'";
+        }
+        
+        public function close()
+        {
+            $this->connection->close();
+        }
+    }
+    
     class Form {
     
     protected $validate = true;
@@ -55,6 +182,17 @@
     
     protected function saveData(){
         
+        $dataManager = new APIDataManager();
+        $dataManager->connect();
+        
+        $this->errorMsg = $dataManager->error();
+        $results = $dataManager->insertClient($this->data['firstName'],$this->data['lastName'], $this->data['email']);
+        if ($results != "success")
+        {
+            $this->errorMsg = $results;
+        }
+        //if($stmt = )
+        //$this->Redirect($this->action, true);
     }
 
     protected function processPost($array){
@@ -76,9 +214,14 @@
         
         $this->saveData();
     }
+        
+    protected function Redirect($url, $permanent = false)
+    {
+        header('Location: '.$url, true, $permanent ? 301 : 302);
 
-}
-?>
+        exit();
+    }
+}?>
 <!doctype html>
 <html lan="en">
 <head>
@@ -95,19 +238,19 @@
     <section class="welcome">
         <h1>Welcome</h1>
         <p>Welcome to Connecticut Basement Systems New Letter Signup Page!</p>
-        <p>We are running PHP verison <?= phpversion() ?></p>
+        <p>We are running PHP verison <?= phpversion()?></p>
     </section>
     <form id="myForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <section class="form">
         <h2>What's Next?</h2>
-        <p>Fill out convenient form, to receive our monthly newsletter. Which includes updates on all products and services.</p>
+        <p>Fill out our convenient form, to receive our monthly newsletter. Which includes updates on all our products and services.</p>
         <ul class="input-list style-1">
             <li>
                 <?php
                     
                     if($_POST && isset($_POST))
                     {
-                        $form = new Form($_POST, "post", "", true);
+                        $form = new Form($_POST, "post", "test.php", true);
                     }
     
                     if(isset($form) && $form->getErrorMsg()) {
@@ -149,7 +292,6 @@
 </body>
 <script>
     function didClickOnSubmitBtn(evt) {
-        alert("Did click on submit button!");
         document.getElementById("myForm").submit();
     }
     var submitBtn = document.getElementById("submitBtn");
